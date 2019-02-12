e59a80d1a777af6766d3f399a405c9001b1303d3
chatti_nader <chatti.tkd@gmail.com>
2019-02-12 21:58:25 +0100

    List file avec la méthode FilenameFilter

--- src/fr/unice/miage/Main.java
+++ src/fr/unice/miage/Main.java
@@ -1,4 +1,6 @@
 package fr.unice.miage;
+
+import java.util.*;
 import java.io.File;
 import java.io.FilenameFilter;
 
@@ -11,12 +13,44 @@ public class Main {
                 lister(file.getPath());
             }
         } else {
-            System.out.println("[" + f.getPath() + "]");
+            System.out.println("[ " + f.getPath() + " ]");
+        }
+    }
+
+    private void listerFilter (String path) {
+        File f = new File(path);
+        FilenameFilter fl = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if (name.endsWith(".txt")){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+
+        for (File afile: f.listFiles()) {
+            for (String file : afile.list(fl)) {
+                System.out.println("[ " + file + " ]");
+            }
         }
     }
 
     public static void main (String[] args){
         Main m = new Main();
-        m.lister("C:/Users/'Chatti Nader/Documents/M1/S2/Prog. Avancée");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choisissez la méthode de recherche: ");
+        System.out.println("1- List Files");
+        System.out.println("2- List Files Filter Name");
+        System.out.print("Votre choix: ");
+        int choix = sc.nextInt();
+
+        if (choix == 1) {
+            m.lister("C:/Users/'Chatti Nader/Documents/M1/S2/Prog. Avancée");
+        } else if (choix == 2) {
+            m.listerFilter("C:/Users/'Chatti Nader/Documents/M1/S2");
+        }
     }
 }
