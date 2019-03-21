e5b3e4c68b5e4188c1f9a0a6c9481cfdce30eae8
chatti_nader <chatti.tkd@gmail.com>
2019-03-21 10:55:13 +0100

    Restructuration des fichier du projet

--- src/fr/unice/miage/TP1/Lister.java
+++ src/fr/unice/miage/TP1/Lister.java
@@ -1,12 +1,11 @@
 package fr.unice.miage.TP1;
 
+import java.io.*;
 import java.util.*;
-import java.io.File;
-import java.io.FilenameFilter;
 
 public class Lister extends ListerFiltrerExterne {
     // --------------------------------------------Lister--------------------------------------------------------------
-    private void lister (String path) {
+    public void lister(String path) {
         File file = new File(path);
 
         // Si c'est un dossier on récupère son path et on rappelle la méthode lister (récursivité)
@@ -22,7 +21,7 @@ public class Lister extends ListerFiltrerExterne {
         }
     }
 // --------------------------------------------Lister Filtrer----------------------------------------------------------
-    private void listerFilter (String path, String ext) {
+public void listerFilter(String path, String ext) {
         File f = new File(path);
 
         // Initialisation du filre (FilenameFilter)
@@ -52,63 +51,6 @@ public class Lister extends ListerFiltrerExterne {
             // Affichage des fichiers qui ont la bonne extension
             if (textFilter.accept(f, f.getName())) {
                 System.out.println("[ " + f.getPath() + " ]");
-            }
-        }
-    }
-
-// --------------------------------------------main--------------------------------------------------------------------
-    public static void main (String[] args){
-        // Initialisation
-        Lister l = new Lister();
-        ListerFiltrerExterne e = new ListerFiltrerExterne();
-        Scanner scInt = new Scanner(System.in);
-        Scanner scString = new Scanner(System.in);
-
-        // Choix de méthode de recherche
-        System.out.println("Choisissez la méthode de recherche: ");
-        System.out.println("1- List Files");
-        System.out.println("2- List Files FilenameFilter");
-        System.out.println("3- List Files FilenameFilter classe extèrne: ");
-        System.out.println("4- List Files FilenameFilter classe anonyme: ");
-
-        // Test sur le choix saisi
-        int choix = 0;
-        do {
-            System.out.print("Votre choix: ");
-            choix = scInt.nextInt();
-        } while (choix <1  || choix > 4);
-
-        // Appel de la méthode en fonction du choix saisi
-        switch (choix) {
-            case 1:
-                l.lister(".");
-                break;
-            case 2: {
-                // Initialisation de l'extension
-                System.out.print("Extension des fichier à afficher (.txt, .java, ..): ");
-                String ext = scString.nextLine();
-                l.listerFilter(".", ext);
-                break;
-            }
-            case 3:{
-                // Initialisation de l'extension
-                System.out.print("Extension des fichier à afficher (.txt, .java, ..): ");
-                String ext = scString.nextLine();
-                e.listerFiltrerExterne(".", ext);
-                break;
-            }
-            case 4: {
-                // Initialisation de l'extension
-                System.out.print("Extension des fichier à afficher (.txt, .java, ..): ");
-                String ext = scString.nextLine();
-                // class anonyme qui appelle la méthode listerFiltrerExterne de la classe ListerFiltrerExterne
-                ListerFiltrerExterne a = new ListerFiltrerExterne(){
-                    public void listerAnonyme () {
-                        ListerFiltrerExterne ea = new ListerFiltrerExterne();
-                        ea.listerFiltrerExterne(".", ext);
-                    }
-                };
-                ((ListerFiltrerExterne) a).listerAnonyme();
             }
         }
     }
