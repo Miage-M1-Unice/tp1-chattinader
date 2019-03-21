dccf180fdc57de64f36a4a5daeb89945d0dc855d
chatti_nader <chatti.tkd@gmail.com>
2019-03-21 16:32:29 +0100

    Appel des méthodes dans la class Main

--- src/fr/unice/miage/TP2/Main.java
+++ src/fr/unice/miage/TP2/Main.java
@@ -6,5 +6,21 @@ import java.io.*;
 public class Main {
     public static void main (String[] args) {
         AnalyseurDeClasse ac = new AnalyseurDeClasse();
+        boolean ok = false;
+
+        while (!ok) {
+            try {
+                System.out.print("Entrez le nom d'une classe (ex : java.util.Scanner): ");
+                String nomClasse = ac.litChaineAuClavier();
+
+                ac.analyseClasse(nomClasse);
+
+                ok = true;
+            } catch (ClassNotFoundException e) {
+                System.out.println("Classe non trouvée.");
+            } catch (IOException e) {
+                System.out.println("Erreur d'E/S!");
+            }
+        }
     }
 }
